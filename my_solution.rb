859fdfc3d405891c17f4b0bc520d48cfda8739aa
Thomas Gracie <tgracie030@gmail.com>
2018-09-12 16:27:16 +0100

    used .index and printed result to find index position for Linlithgow

--- my_solution.rb
+++ my_solution.rb
@@ -14,11 +14,15 @@ stops.insert(0, "Glasgow Queen St")
 
 # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
-p stops
+
 # Work out the index position of "Linlithgow"
+stops.index("Linlithgow")
+
 # Remove "Livingston" from the array using its name
 # Delete "Cumbernauld" from the array by index
 # How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
 # Print out all the stops using a for loop
+
+p stops.index("Linlithgow")
