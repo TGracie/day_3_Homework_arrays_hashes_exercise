b4f73b1386bd53d09ab0e5ab579b07730e05c7a5
Thomas Gracie <tgracie030@gmail.com>
2018-09-12 20:41:07 +0100

    Exercise C Q2. cmd shift D on England and changed values to respective NI data. Remembered comma between hashes and printed out full array/hash.

--- my_solution.rb
+++ my_solution.rb
@@ -1,10 +1,10 @@
-stops = [ "Croy",
-  "Cumbernauld",
-  "Falkirk High",
-  "Linlithgow",
-  "Livingston",
-  "Haymarket"
-]
+# stops = [ "Croy",
+#   "Cumbernauld",
+#   "Falkirk High",
+#   "Linlithgow",
+#   "Livingston",
+#   "Haymarket"
+# ]
 
 # Add "Edinburgh Waverley" to the end of the array
 # stops.push("Edinburgh Waverley")
@@ -43,61 +43,61 @@ stops = [ "Croy",
 
 
 #EXERCISE B
-users = {
-  "Jonathan" => {
-    :twitter => "jonnyt",
-    :lottery_numbers => [6, 12, 49, 33, 45, 20],
-    :home_town => "Stirling",
-    :pets => [
-    {
-      :name => "fluffy",
-      :species => "cat"
-    },
-    {
-      :name => "fido",
-      :species => "dog"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    }
-  ]
-  },
-  "Erik" => {
-    :twitter => "eriksf",
-    :lottery_numbers => [18, 34, 8, 11, 24],
-    :home_town => "Linlithgow",
-    :pets => [
-    {
-      :name => "nemo",
-      :species => "fish"
-    },
-    {
-      :name => "kevin",
-      :species => "fish"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    },
-    {
-      :name => "rupert",
-      :species => "parrot"
-    }
-  ]
-  },
-  "Avril" => {
-    :twitter => "bridgpally",
-    :lottery_numbers => [12, 14, 33, 38, 9, 25],
-    :home_town => "Dunbar",
-    :pets => [
-      {
-        :name => "monty",
-        :species => "snake"
-      }
-    ]
-  }
-}
+# users = {
+#   "Jonathan" => {
+#     :twitter => "jonnyt",
+#     :lottery_numbers => [6, 12, 49, 33, 45, 20],
+#     :home_town => "Stirling",
+#     :pets => [
+#     {
+#       :name => "fluffy",
+#       :species => "cat"
+#     },
+#     {
+#       :name => "fido",
+#       :species => "dog"
+#     },
+#     {
+#       :name => "spike",
+#       :species => "dog"
+#     }
+#   ]
+#   },
+#   "Erik" => {
+#     :twitter => "eriksf",
+#     :lottery_numbers => [18, 34, 8, 11, 24],
+#     :home_town => "Linlithgow",
+#     :pets => [
+#     {
+#       :name => "nemo",
+#       :species => "fish"
+#     },
+#     {
+#       :name => "kevin",
+#       :species => "fish"
+#     },
+#     {
+#       :name => "spike",
+#       :species => "dog"
+#     },
+#     {
+#       :name => "rupert",
+#       :species => "parrot"
+#     }
+#   ]
+#   },
+#   "Avril" => {
+#     :twitter => "bridgpally",
+#     :lottery_numbers => [12, 14, 33, 38, 9, 25],
+#     :home_town => "Dunbar",
+#     :pets => [
+#       {
+#         :name => "monty",
+#         :species => "snake"
+#       }
+#     ]
+#   }
+# }
 
 # Get Jonathan's Twitter handle (i.e. the string "jonnyt")
 # p users["Jonathan"][:twitter]
@@ -134,16 +134,51 @@ users = {
 # p users["Erik"][:pets].push(:dog, "Fluffy")
 
 # Add another person to the users hash
-users["Thomas"] = {
-  :twitter => "Doesn't have it",
-  :lottery_numbers => [3, 18, 26, 58, 9, 35],
-  :home_town => "Edinburgh",
-  :pets => [
-    {
-      :name => "Maggie",
-      :species => "Dog"
-    }
-  ]
-}
+# users["Thomas"] = {
+#   :twitter => "Doesn't have it",
+#   :lottery_numbers => [3, 18, 26, 58, 9, 35],
+#   :home_town => "Edinburgh",
+#   :pets => [
+#     {
+#       :name => "Maggie",
+#       :species => "Dog"
+#     }
+#   ]
+# }
+#
+# p users["Thomas"]
 
-p users["Thomas"]
+#Exercise C
+
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  },
+  {
+    name: "Northern Ireland",
+    population: 1811000,
+    capital: "Belfast"
+  }
+]
+
+# Change the capital of Wales from "Swansea" to "Cardiff".
+
+p united_kingdom
+
+# Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+
+
+# Use a loop to print the names of all the countries in the UK.
+# Use a loop to find the total population of the UK.
