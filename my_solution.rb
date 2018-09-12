2624295d9867850f2545f433ab4263a61ca4e999
Thomas Gracie <tgracie030@gmail.com>
2018-09-12 16:24:34 +0100

    added Polmont at index 4 using insert function

--- my_solution.rb
+++ my_solution.rb
@@ -13,7 +13,7 @@ stops.push("Edinburgh Waverley")
 stops.insert(0, "Glasgow Queen St")
 
 # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-# stops.insert(3, "Polmont")
+stops.insert(4, "Polmont")
 p stops
 # Work out the index position of "Linlithgow"
 # Remove "Livingston" from the array using its name
