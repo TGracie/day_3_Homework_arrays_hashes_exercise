0bf601abb6b4cb408787b49e8bbc9ab4566ec80d
Thomas Gracie <tgracie030@gmail.com>
2018-09-12 16:22:13 +0100

    Exercise A, Added Waverley to stop list using .push

---
+++ my_solution.rb
@@ -0,0 +1,24 @@
+stops = [ "Croy",
+  "Cumbernauld",
+  "Falkirk High",
+  "Linlithgow",
+  "Livingston",
+  "Haymarket"
+]
+
+# Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+
+# Add "Glasgow Queen St" to the start of the array
+# stops.insert(0, "Glasgow Queen St")
+
+# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# stops.insert(3, "Polmont")
+p stops
+# Work out the index position of "Linlithgow"
+# Remove "Livingston" from the array using its name
+# Delete "Cumbernauld" from the array by index
+# How many stops there are in the array?
+# How many ways can we return "Falkirk High" from the array?
+# Reverse the positions of the stops in the array
+# Print out all the stops using a for loop
